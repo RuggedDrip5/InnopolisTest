503eb96e19100fc8c856b7444ecbe54fe0a90f49
RuggedDrip5 <afinoshka@gmail.com>
2024-06-02 23:52:26 +0300

    Add Home2 2 work Add Home2 issue 3* Add Home2 issue 1 FahrenheitToCelsius import static java.lang.System.out

--- ru.innopolis.java.homework/src/Home2/FahrenheitToCelsius.java
+++ ru.innopolis.java.homework/src/Home2/FahrenheitToCelsius.java
@@ -1,9 +1,11 @@
 package Home2;
 
+import static java.lang.System.out;
+
 public class FahrenheitToCelsius {
     public static void main(String[] args) {
         double fahrenheit = 212.0;
         double celsius = (fahrenheit - 32) / 1.8;
-        System.out.println("Температура в градусах Цельсия: " + celsius);
+        out.println("Температура в градусах Цельсия: " + celsius);
     }
 }
