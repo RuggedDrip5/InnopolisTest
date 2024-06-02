0932449272fadef9385abf413f83376d023b0301
RuggedDrip5 <afinoshka@gmail.com>
2024-06-02 23:16:09 +0300

    First step repogittest

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+A
+src/src/Main.java,5\c\5cd9529137dd62a6d2acb5d098a8e30a10c55d92
+X
+(ru.innopolis.java.homework/src/Main.java,d\b\dbfec879777195f7099c9c9442cf4cc1f33d3f17
+a
+1ru.innopolis.java.homework/src/Home1/numbers.java,3\d\3d82b59227e280e7c6e521b85253852b2b6af1e7
---
+++ ru.innopolis.java.homework/src/Home1/numbers.java
@@ -0,0 +1,15 @@
+package Home1;
+
+
+ public class numbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 6;
+        int num2 = 22;
+        int num3 = 103;
+        int num4 = 405;
+
+        System.out.printf("%d   %d   %d   %d%n", num1, num2, num3, num4);
+    }
+}
