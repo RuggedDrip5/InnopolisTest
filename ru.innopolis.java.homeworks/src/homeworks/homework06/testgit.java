f12f5b8076d6b13310587f106ed65579ba4f99c5
ruggeddrip5 <afinoshka@gmail.com>
2024-07-06 00:29:28 -0700

    refactor directories

---
+++ ru.innopolis.java.homeworks/src/homeworks/homework06/testgit.java
@@ -0,0 +1,11 @@
+package homeworks.homework06;
+
+import static java.lang.System.out;
+
+public class testgit {
+    public static void main(String[] args) {
+        int i = 10;
+        out.print(i);
+    }
+}
+
